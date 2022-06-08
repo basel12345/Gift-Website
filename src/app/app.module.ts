16cfc23cc32a2a52b9b34f96503f9537fb88d704
mahmoud.hamza <mahmoud.hamza@3ddx.com>
2022-06-08 23:26:41 +0200

    about & contact styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,24 +11,26 @@ import { AuthGuard } from './shared/guard/auth.guard';
 import { ToastrModule } from 'ngx-toastr';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    RouterModule,
-    HttpClientModule,
-    ToastrModule.forRoot({
-      progressBar: true,
-      timeOut: 3000,
-      positionClass: 'toast-top-center',
-      closeButton: true,
-      tapToDismiss: true,
-      enableHtml: true,
-    }),
-    BrowserAnimationsModule,
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        RouterModule,
+        HttpClientModule,
+        ToastrModule.forRoot({
+            progressBar: true,
+            timeOut: 3000,
+            positionClass: 'toast-top-center',
+            closeButton: true,
+            tapToDismiss: true,
+            enableHtml: true,
+        }),
+        BrowserAnimationsModule,
+        SharedModule,
+    ],
   providers: [AuthGuard, ApisService, interceptorProviders],
   bootstrap: [AppComponent],
 })
