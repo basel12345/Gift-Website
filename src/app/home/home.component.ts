efdf78c34bb1f8e0fa07fe8858214077e7a4be78
mahmoud.hamza <mahmoud.hamza@3ddx.com>
2022-06-11 19:01:53 +0200

    home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ApisService} from "../shared/services/apis.service";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+data: any;
+  constructor(private api: ApisService) {
   }
 
+  ngOnInit(): void {
+    this.getGiftList();
+    console.log(this.data, 'init')
+  }
+  getGiftList() {
+    this.api.GET('api/v1/home').subscribe((res: any) => {
+      console.log("response", res.body);
+      if (res.body.success === true){
+      this.data = res.body.data
+      console.log('a')
+      }
+    });
+  }
 }
