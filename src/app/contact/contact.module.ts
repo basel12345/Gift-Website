42bae3193e34ac740bc0061a5499edefd45c2229
mahmoud.hamza <mahmoud.hamza@3ddx.com>
2022-06-11 03:24:09 +0200

    contact validations

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -5,15 +5,17 @@ import { CommonModule } from '@angular/common';
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from './contact/contact.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MaterialModule} from "../shared/material/material/material.module";
 
 @NgModule({
   declarations: [ContactComponent],
-  imports: [
-    CommonModule,
-    ContactRoutingModule,
-    SharedModule,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
+    imports: [
+        CommonModule,
+        ContactRoutingModule,
+        SharedModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MaterialModule,
+    ],
 })
 export class ContactModule {}
