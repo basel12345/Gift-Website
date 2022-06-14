096e0a8c0af77e24c4dc418d6481331ac3f9e7f5
mahmoud.hamza <mahmoud.hamza@3ddx.com>
2022-06-14 02:16:16 +0200

    public events

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,20 +8,35 @@ import {ApisService} from "../shared/services/apis.service";
 })
 export class HomeComponent implements OnInit {
 data: any;
+publicEvents = [];
+privateEvents = [];
+viewedEvents = [];
+publicCartegories = [];
   constructor(private api: ApisService) {
   }
 
   ngOnInit(): void {
     this.getGiftList();
-    console.log(this.data, 'init')
+    this.getEventsTypes();
   }
   getGiftList() {
     this.api.GET('api/v1/home').subscribe((res: any) => {
-      console.log("response", res.body);
       if (res.body.success === true){
-      this.data = res.body.data
-      console.log('a')
+      this.data = res.body.data;
+     this.viewedEvents = this.data.events;
+     this.getEventsTypes();
+     this.publicCartegories = [...new Set( this.publicEvents?.map(res => {return res.category.name}))];
       }
     });
+  }
+  getEventsTypes(): any{
+    this.publicEvents = this.data?.events?.filter(res => res.type === 'public');
+    this.privateEvents = this.data?.events?.filter(res => res.type === 'private')
+  }
+
+  getEventsViewed(name: string) {
+   this.viewedEvents = this.publicEvents.filter( res => res.category.name === name)
+    console.log(this.viewedEvents, 'view')
+    return false
   }
 }
