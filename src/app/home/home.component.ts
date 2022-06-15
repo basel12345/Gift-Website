3afae76caedba5701ca22465f5e1d323e12933c1
unknown <basel.elsawy@3ddx.com>
2022-06-15 23:51:43 +0200

    slider

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ApisService} from "../shared/services/apis.service";
+import { ApisService } from "../shared/services/apis.service";
 
 @Component({
   selector: 'app-home',
@@ -7,7 +7,8 @@ import {ApisService} from "../shared/services/apis.service";
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-data: any;
+  data: any;
+  number: number = 0;
   constructor(private api: ApisService) {
   }
 
@@ -18,10 +19,20 @@ data: any;
   getGiftList() {
     this.api.GET('api/v1/home').subscribe((res: any) => {
       console.log("response", res.body);
-      if (res.body.success === true){
-      this.data = res.body.data
-      console.log('a')
+      if (res.body.success === true) {
+        this.data = res.body.data
+        console.log(this.data)
       }
     });
+  }
+
+  slider(type: string) {
+    if(type === 'next') {
+      this.number += 1;
+      if(this.data.slider.length === this.number) this.number = 0;
+    } else {
+      this.number -= 1;
+      if(this.number === -1) this.number = this.data.slider.length - 1;
+    };
   }
 }
