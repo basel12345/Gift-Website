d7e0ec51997e603b300168fd3c817c0e87f62553
mahmoud.hamza <mahmoud.hamza@3ddx.com>
2022-06-28 22:51:16 +0200

    links header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApisService} from "./shared/services/apis.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'website';
+  headerLinks = [];
+  footerLinks = [];
+  constructor(private api: ApisService) {
+  }
+  ngOnInit() {
+    this.api.GET('api/v1/list/pages').subscribe((res: any) => {
+      if (res.body.success === true) {
+        this.headerLinks = res.body.data.filter(res => res.position === "header");
+        this.footerLinks = res.body.data.filter(res => res.position === "footer");
+      };
+    });
+  }
 }
+
